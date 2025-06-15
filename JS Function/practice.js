4d3ab1b87057bbec7e13413e98def1530e1723f1
Samruddhi Jagdale <samruddhijagdale2000@gmail.com>
2025-06-15 13:17:11 +0530

    Added DOM folder

--- JS Function/practice.js
+++ JS Function/practice.js
@@ -1,3 +1,10 @@
 //Given the string "LearnJavaScript", extract the first 10 characters.
-var string = "LearnJavaScript";
-console.log(string.slice(0,10)); 
+//var string = "LearnJavaScript";
+//console.log(string.slice(0,10)); 
+
+
+const names=["James","Jane","Doe","Roni"];
+
+const filterValues = names.filter((element) => {
+    console.log(element.length < 4);
+    })
